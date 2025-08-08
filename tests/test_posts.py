3d1d3b424f97c2c270d07681b6e0417e56c3b33e
Hefi92 <85995856+Hefi92@users.noreply.github.com>
2025-08-08 11:03:53 +0200

    Testing_post_method.py

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,10 +1,47 @@
+import pytest
 import requests
 
-BASE_URL = "https://jsonplaceholder.typicode.com"
+def test_post():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    payload = {
+        "title": "Learning testing API.",
+        "body": "POST method verification.",
+        "userId": 3
+    }
 
-def test_get_all_posts():
-    response = requests.get(f"{BASE_URL}/posts")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-    assert len(response.json()) > 0
+    response = requests.post(url, json=payload)
+    assert response.status_code == 201
+    data = response.json()
 
+    assert isinstance(data, dict)
+    assert data["userId"] == 3
+
+def test_post_1():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    payload = {
+        "title": "Data adding to post '1'.",
+        "body": "Check the conditions by 'assert'.",
+        "userId": 1
+    }
+
+    response = requests.post(url, json=payload)
+    assert response.status_code == 201
+    data = response.json()
+
+    assert data["id"] == 101
+    assert data["userId"] == payload["userId"]
+
+    def test_post_9999():
+        payload = {
+        "title": "Testing error scenario.",
+        "body": "This test will lead to error.",
+        "userId": 9999
+    }
+
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.post(url, json=payload)
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["title"] == payload["title"]
